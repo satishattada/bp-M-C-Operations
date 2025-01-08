5cafb826fdfda932620cf7cba36aa6832c726e3c
adhavan.s_g <adhavan.s_g@bp.com>
2025-01-08 18:46:10 +0530

    added git ignore

--- src/app/enums/routeEnum.tsx
+++
@@ -1,7 +0,0 @@
-export enum RoutesEnum {
-    DASHBOARD = "/",
-    TEAM_MANAGEMENT = "/team-management",
-    FINANCE_MANAGEMENT = "/finance-management",
-    DEMAND_MANAGEMENT = "/demand-management",
-    
-  }
